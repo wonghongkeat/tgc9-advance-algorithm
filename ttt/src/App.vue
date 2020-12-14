33e13728e0a2c8b7524ba58d05af878653833bd4
wonghongkeat <hongkeat1991@gmail.com>
2020-12-14 07:12:18 +0000

    edited

---
+++ ttt/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+      <Board/>
+  </div>
+</template>
+
+<script>
+import Board from './components/Board'
+
+export default {
+  name: 'App',
+  components: {
+      Board
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ ttt/src/components/Board.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+    <table>
+        <tr v-for='(each_row, rindex) in board' v-bind:key="rindex">
+            <td v-for='(each_col, cindex) in each_row' v-bind:key='rindex*3+cindex'>
+                {{each_col}}
+            </td>
+        </tr>
+    </table>
+</div>
+</template>
+
+<script>
+export default {
+    data:function(){
+        return {
+            board:[
+                ['', '', ''],
+                ['', '', ''],
+                ['', '', '']
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+td {
+    width: 100px;
+    height: 100px;
+    font-size: 32px;
+    border:1px solid black;
+    text-align:center;
+}
+</style>
